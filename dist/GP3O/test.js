6afc292faf5056039e9e1538add4337019a0e079
Jake Daurham <daurham95@gmail.com>
2023-09-01 02:02:26 -0700

    setting up on pi

--- dist/GP3O/test.js
+++ dist/GP3O/test.js
@@ -1,9 +1,23 @@
 import schedule from 'node-schedule';
 import { LED } from "./GPIOModule.js";
 import { scheduleRules } from '../dicts.js';
+
+console.log('Test running');
+
 // Blink every 10 seconds
 const blinkTest = () => {
     const RedLED = new LED(17);
-    RedLED.blink();
+    RedLED.on();
+	//RedLED.blink(10000);
 };
-schedule.scheduleJob(scheduleRules.Every10Seconds, blinkTest);
+const mod = new LED(17, 'out');
+console.log(mod.gpio.readSync())
+console.log(mod.on());
+console.log(mod.gpio.readSync());
+console.log(mod.off());
+console.log(mod.gpio.readSync());
+//blinkTest();
+//schedule.scheduleJob(scheduleRules.Every10Seconds, () => {
+//	console.log('scheduler running');
+//	blinkTest();
+//});
